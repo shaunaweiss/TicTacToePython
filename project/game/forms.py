81ed88ed472b38ddb53a2946e03bd7e77ae6a218
Lucas Harris <harrislr@miamioh.edu>
2019-04-04 11:49:16 -0400

    got view working, added some form functionality in the views.py

--- project/game/forms.py
+++ project/game/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class BoardForm(forms.Form):
     square = forms.ChoiceField(choices=(('0', '0'), ('1', '1'), ('2', '2'),
-    ('3', '3'), ('4', '4'), ('5', '5'),('6', '6'), ('7', '7'), ('8', '8')))
+    ('3', '3'), ('4', '4'), ('5', '5'),('6', '6'), ('7', '7'), ('8', '8')),
+    widget = forms.RadioSelect(attrs={'onchange': 'this.form.submit();'}))
--- project/game/views.py
+++ project/game/views.py
@@ -1,13 +1,28 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 import game
 from game.forms import BoardForm
+from game.models import *
+from datetime import *
 
 
 def index(request):
+    # board = Board.objects.create(pub_date=datetime.now(), game_name="yes")
+    # board.save()
+    # for i in range(8,9):
+    #     space = Space.objects.create(id=i, board_space=Board.objects.get(id=1), space_value=1)
+    #     space.save()
+    if request.method == "POST":
+        board_form = BoardForm(request.POST)
+        if board_form.is_valid():
+            board = Board.objects.get(id=1)
+            space = Space.objects.get(board_space=1, id=board_form.cleaned_data['square'])
+            space.save()
+            return HttpResponseRedirect('/')
+        else:
+            board_form = BoardForm()
     board_form = BoardForm()
     return render(request, 'game/index.html', {'board_form':board_form})
 
-# def get_board(request):
-#     board = game.
